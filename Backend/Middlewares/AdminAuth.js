2c097664d69d4e0ac6e361a4cad98db9ebd50597
Gautam Sonkar <gautamsonkar334@gmail.com>
2023-02-22 23:56:09 +0530

    day2

--- Backend/Middlewares/AdminAuth.js
+++ Backend/Middlewares/AdminAuth.js
@@ -1,7 +1,13 @@
-const bcrypt=require("bcrypt")
-require('dotenv')
+const jwt=require("jsonwebtoken")
+require('dotenv').config()
 const AdminAuth=(req,res,next)=>{
     const token=req.headers.authorization
+    if(token){
+        const decode=jwt.verify(token,process.env.secretKey)
+        if(decode){
+            -
+        }
+    }
     const email=req.body.email
     if(email=="sonkarg117@gmail.com"){
         next()
