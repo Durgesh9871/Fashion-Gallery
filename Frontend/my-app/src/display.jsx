aa2815b159b138b42e68cccf9b258e9e5cd28208
Jagroshan <jagroshansingh615@gmail.com>
2023-02-25 07:32:35 +0530

    CartOrder

--- Frontend/my-app/src/display.jsx
+++ Frontend/my-app/src/display.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { CartPage } from './Jagroshan/Pages/CartPage'
 import { ProductPage } from './Jagroshan/Pages/ProductPage'
 
 const DisplayJavascript = () => {
   return (
     <div>
-      <ProductPage/>
+      {/* <ProductPage/> */}
+      <CartPage/>
     </div>
   )
 }
