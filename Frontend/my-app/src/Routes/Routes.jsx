484013adf83ed75c8e18d230e18b476e242b15e7
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-26 11:07:06 +0530

    file

--- Frontend/my-app/src/Routes/Routes.jsx
+++ Frontend/my-app/src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import {Routes , Route} from "react-router-dom"
+import { MainAdminPage } from '../Durgesh_Folder/Pages/MainAdminPage'
 import { MainHomePage } from '../Durgesh_Folder/Pages/MainHomePage'
 
 
