1789940a2ebe8be8dfe31ff476570b8841e95f30
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-26 11:12:06 +0530

    file

--- Frontend/my-app/src/index.tsx
+++ Frontend/my-app/src/index.tsx
@@ -5,8 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import {store} from "./Redux/store"
+import { Provider } from 'react-redux';
 
 
 
