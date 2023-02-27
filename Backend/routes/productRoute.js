00cc1d7551ce349a2ec011e5a387aa57abf98050
Gautam Sonkar <gautamsonkar334@gmail.com>
2023-02-27 09:47:51 +0530

    get

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -61,27 +61,80 @@ ProductsRoute.get("/:id", async (req, res) => {
 
 //GET ALL ,Product, Anyone can access
 
+// ProductsRoute.get("/", async (req, res) => {
+//   const qNew = req.query.new;
+//   console.log({qNew})
+//   const qCategory = req.query.category;
+//   console.log({qCategory})
+//   try {
+//     let products;
+
+//     if (qNew) {
+//       products = await ProductModel.find().sort({ createdAt: -1 }).limit(10);
+//     } else if (qCategory) {
+//       products = await ProductModel.find({
+//         categories:{
+//           $in:[qCategory],
+//         },
+//       });
+//     } else {
+//       products = await ProductModel.find();
+//     }
+
+//     res.status(200).send(products);
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
+
 ProductsRoute.get("/", async (req, res) => {
+  const query=req.body
   const qNew = req.query.new;
-  console.log({qNew})
   const qCategory = req.query.category;
   console.log({qCategory})
+  const price=req.query.price
+  const color=req.query.color
+  const sort=req.query.sort
   try {
     let products;
-
-    if (qNew) {
-      products = await ProductModel.find().sort({ createdAt: -1 }).limit(10);
-    } else if (qCategory) {
-      products = await ProductModel.find({
-        categories:{
-          $in:[qCategory],
-        },
-      });
-    } else {
-      products = await ProductModel.find();
+   
+    if(price!==undefined &&color!==undefined && qCategory!==undefined){
+      products=await ProductModel.find({$and:[{categories:{$in:[qCategory]}},{price:{$lte:price}},{color:{$regex:`${color}`,$options:"i"}}]})
     }
-
-    res.status(200).send(products);
+    else if(qCategory!==undefined && price!==undefined){
+      products=await ProductModel.find({$and:[{categories:{$in:[qCategory]}},{price:{$lte:price}}]})
+    }
+    else if(qCategory!==undefined&&color!==undefined){
+      products=await ProductModel.find({$and:[{categories:{$in:[qCategory]}},{color:{$regex:`${color}`,$options:"i"}}]})
+    }
+    else if(color!==undefined && price!==undefined){
+      products=await ProductModel.find({$and:[{price:{$lte:price}},{color:{$regex:`${color}`,$options:"i"}}]})
+    }
+    else {
+      if(qCategory!==undefined){
+        products = await ProductModel.find({
+            categories:{
+              $in:[qCategory],
+            },
+        });
+      }
+      else if(color!==undefined){
+        products=await ProductModel.find({color})
+      }
+      else if(price!==undefined){
+        products=await ProductModel.find({price})
+      }
+    }
+    
+   if(sort!==undefined){
+    if(sort=='asc'){
+      products.sort((a,b)=>a.price-b-price)
+    }
+    else{
+      products.sort((a,b)=>b.price-a.price)
+    }
+   }
+    products.length>0?res.status(200).send(products):res.status(200).send("There is no products")
   } catch (err) {
     res.status(500).send(err);
   }
