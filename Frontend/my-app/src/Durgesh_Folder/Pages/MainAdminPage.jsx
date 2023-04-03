10223eaa7596f73ca41a29e0f4023e04c311bbb2
Durgesh9871 <devisingh9871686098@gmail.com>
2023-04-03 12:55:03 +0530

    file

--- Frontend/my-app/src/Durgesh_Folder/Pages/MainAdminPage.jsx
+++ Frontend/my-app/src/Durgesh_Folder/Pages/MainAdminPage.jsx
@@ -42,9 +42,13 @@ useEffect(()=>{
    <Box backgroundColor="#171923" border="2px   red" height="100vh">
        
        {/*  Started from here ------- */}
-   <Box width={{ base: "100%", sm: "100%", md: "100%", lg: "100%", xl: "81%", '2xl': "81%" }} border="1px solid red" height="500px" marginLeft="auto"  >
-     <Text color="#ffffff" textAlign="center" fontSize="27px" fontWeight={500} >Admin <span style={{color:"greenyellow"}}>Dashboard</span></Text>
-  
+   <Box width={{ base: "100%", sm: "100%", md: "100%", lg: "100%", xl: "81%", '2xl': "81%" }} border="1px   red" height="500px" marginLeft="auto" padding="20px" mt="-60px" >
+     <Text color="#ffffff" textAlign="center" fontSize="29px" fontWeight={500} >Admin <span style={{color:"greenyellow"}}>Dashboard</span></Text>
+   
+   <Box background="#2e2e2e" width="100%" height="150px" mt="30px" p="10px" >
+    <Text textAlign="center" fontWeight={500} color="greenyellow" fontSize="40px" >Total Sale</Text>
+       <Text textAlign="center" fontWeight={500} color="black" fontSize="38px">$ 500</Text>
+   </Box>
   
      
    </Box>
