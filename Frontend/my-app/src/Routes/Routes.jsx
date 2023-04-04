28b8d88a619b1997ca9ac2098af6bb8c473dc96c
Durgesh9871 <devisingh9871686098@gmail.com>
2023-04-04 10:18:23 +0530

    file

--- Frontend/my-app/src/Routes/Routes.jsx
+++ Frontend/my-app/src/Routes/Routes.jsx
@@ -19,6 +19,7 @@ const AllRoutes = () => {
       <Route path="/customerPageAdmin" element={<CustomerPage />}></Route>
       <Route path="/deltePageAdmin" element={<DeletePage />}></Route>
       <Route path="/addPageAdmin" element={<AddProduct />}></Route>
+      <Route path="/editPageAdmin" element={<AddProduct />}></Route>
       <Route path="/stat" element={<Stats />}></Route>
       <Route path="/products" element={<ProductPage/>}/>
       <Route path="/cart" element={<CartPage/>}/>
