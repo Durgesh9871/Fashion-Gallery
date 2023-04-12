7884817872f03984e766a400714b50379aec66a7
Durgesh9871 <devisingh9871686098@gmail.com>
2023-04-12 09:10:53 +0530

    file

--- Frontend/my-app/src/Jagroshan/Components/Pagination.jsx
+++ Frontend/my-app/src/Jagroshan/Components/Pagination.jsx
@@ -1,15 +1,55 @@
-import { Box, Button, Heading, HStack } from '@chakra-ui/react'
-import React from 'react'
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
+import { Box, Button, Heading, HStack , Text } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export const Pagination = () => {
-  const [page,setpage]=React.useState(1)
+  const [page , setPage] = useState(1)
+  const [pageNext , setPageNext] = useState(false)
+const [pagePre , setPagePre] = useState(false)
+const [paginationData , setPaginationData] = useState([])
+
+
+//  GET ALL DATA FOR PAGINATION FOR SETTING DISABLE BUTTON 
+const PaginationFunction = ()=>{
+  axios.get(`https://long-pear-giraffe-gown.cyclic.app/api/mens`)
+  .then((res)=> setPaginationData(res.data))
+}
+
+useEffect(()=>{
+  PaginationFunction()
+},[])
+  
+      
+const handleNextPage = (data)=>{
+  setPageNext(true)
+  setPage(page+data)
+  setTimeout(()=>{
+    setPageNext(false)
+  },400)
+  
+ }
+ const handlePreviosPage = (data)=>{
+  setPagePre(true)
+  setPage(page+data)
+  setTimeout(()=>{
+    setPagePre(false)
+  },400)
+ }
+
+
+ const nextPageDisable = Math.ceil(paginationData.length/9)
+
+
   return (
-    <div>
-      <HStack justifyContent={'center'}>
-        <Button onClick={()=>setpage((next)=>next-1)} isDisabled={page==0?true:false}>Prev</Button>
-        <Heading size={'sm'}>{page}</Heading>
-        <Button onClick={()=>setpage((next)=>next+1)}>Next</Button>
-      </HStack>
-    </div>
+    <Box style={{display:"flex" , justifyContent:"center"}}  padding="60px 0px" >
+
+    <Button onClick={()=>handlePreviosPage(-1)} isLoading={pagePre} isDisabled={page==1}   ml="3"  pl={{base:"5px", sm: "10px", md: "10px", lg: "10px",xl: "10px",'2xl': "10px"}} pr={{base:"10px", sm: "10px", md: "10px", lg: "10px",xl: "10px",'2xl': "10px"}} > 
+   <Text fontSize="22px" fontWeight="400"><ChevronLeftIcon fontSize="26px" ml="-8px" p="0"/>Prev</Text></Button>
+    <Button ml="3" fontSize="22px" fontWeight="400"> {page} </Button>
+    <Button onClick={()=>handleNextPage(1)} isLoading={pageNext} isDisabled={page == nextPageDisable}  ml="3" pl={{base:"10px", sm: "10px", md: "10px", lg: "10px",xl: "10px",'2xl': "10px"}} pr={{base:"5px", sm: "2px", md: "2px", lg: "2px",xl: "2px",'2xl': "2px"}}  > 
+   <Text fontSize="22px" fontWeight="400">Next <ChevronRightIcon fontSize="26px" ml="-8px" p="0"/></Text> </Button>
+ 
+  </Box>
   )
 }
