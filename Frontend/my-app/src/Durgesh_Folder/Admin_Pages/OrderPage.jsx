d33f1efe6904b8c40bae0b600e164407e6399056
Durgesh9871 <devisingh9871686098@gmail.com>
2023-04-13 18:11:39 +0530

    file

--- Frontend/my-app/src/Durgesh_Folder/Admin_Pages/OrderPage.jsx
+++ Frontend/my-app/src/Durgesh_Folder/Admin_Pages/OrderPage.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
 import React from 'react'
 import { Sidebar } from '../Admin_Components/SliderAdmin'
 
@@ -10,6 +10,10 @@ const OrderPage = () => {
 
             {/* Order Details  STARTED FROM HERE --------------------- */}
             <Box width={{ base: "100%", sm: "100%", md: "100%", lg: "100%", xl: "81%", '2xl': "81%" }} border="1px   red" height="auto" marginLeft="auto"  p="2%" mt="-60px">
+             
+            <Heading color="#fff" fontWeight="500">
+            Customers
+          </Heading>
 
             </Box>
           </Box>
