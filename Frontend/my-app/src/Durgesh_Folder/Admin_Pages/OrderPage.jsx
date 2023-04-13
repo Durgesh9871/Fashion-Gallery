13c5f6d70acdbe96d39f796951a89af514df7e64
Durgesh9871 <devisingh9871686098@gmail.com>
2023-04-13 18:12:09 +0530

    file

--- Frontend/my-app/src/Durgesh_Folder/Admin_Pages/OrderPage.jsx
+++ Frontend/my-app/src/Durgesh_Folder/Admin_Pages/OrderPage.jsx
@@ -12,7 +12,7 @@ const OrderPage = () => {
             <Box width={{ base: "100%", sm: "100%", md: "100%", lg: "100%", xl: "81%", '2xl': "81%" }} border="1px   red" height="auto" marginLeft="auto"  p="2%" mt="-60px">
              
             <Heading color="#fff" fontWeight="500">
-            Customers
+            Order Details & Status
           </Heading>
 
             </Box>
