b47953d495b395268b8f14ceeb69d6da6435b8bf
Gautam Sonkar <gautamsonkar334@gmail.com>
2023-04-20 20:14:36 +0530

    gg

--- Backend/index.js
+++ Backend/index.js
@@ -1,39 +1,10 @@
 
-const express=require("express")
-require("dotenv").config()
-port=process.env.port
-const {connection}=require("./configs/db")
-
-const {ProductsRoute}=require("./routes/Products")
-
-app=express()
-
-app.use(express.json())
-app.use('/products',ProductsRoute)
-app.get('/' , (req , res)=>{
-
-   res.send('hello from simple server :)')
-
-})
-
-app.listen(port,async()=>{
-    try{
-        await connection
-        console.log("connected to db")
-    }
-    catch(err){
-        console.log(err)
-    }
-    console.log("listening at port",port)
-}) 
 
 const express = require("express");
 require("dotenv").config();
 const { connection } = require("./config/db");
 const { userRouter } = require("./routes/User.route");
 const {ProductsRoute}=require('./routes/productRoute');
-const {OrderRouter}=require('./routes/Order');
-const {Cartrouter}=require('./routes/Cart');
 const {authenticate}=require('./middlewares/Auth.middleware')
 
 const cors = require("cors");
@@ -46,9 +17,6 @@ app.use(express.json());
 
 app.use("/users", userRouter);
 app.use('/products',ProductsRoute);
-app.use(authenticate);
-app.use('/orders',OrderRouter);
-app.use('/carts',Cartrouter);
 
 //connect to the server:-
 app.listen(process.env.port, async () => {
