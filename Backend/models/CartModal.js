0e9fd17da23fe86bd81b745fe8e4545390025b93
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-04-24 02:18:08 +0530

    Update CartModal.js

--- Backend/models/CartModal.js
+++ Backend/models/CartModal.js
@@ -1,3 +1,32 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema(
+  {
+
+    userId: { 
+      type:mongoose.Schema .ObjectId , 
+      required: false ,
+      ref:"user"
+    },
+    productId:{ 
+      type:mongoose.Schema .ObjectId, 
+      required: true ,
+      ref:"Product"
+    },
+    quantity: {
+          type: Number,
+          default: 1,
+        }
+  },
+  { timestamps: true }
+);
+const CartModel=mongoose.model("Cart", CartSchema);
+
+module.exports = {
+    CartModel
+}
+
+
 
 
 
