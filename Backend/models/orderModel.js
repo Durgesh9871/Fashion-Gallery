b3769e653f52990b4e4bc0ebc1841b76f5076669
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-04-25 10:51:20 +0530

    Update orderModel.js

--- Backend/models/orderModel.js
+++ Backend/models/orderModel.js
@@ -1,5 +1,37 @@
 const mongoose = require("mongoose");
 
+const OrderSchema = new mongoose.Schema(
+  {
+
+    userId: { 
+      type: mongoose.Schema.ObjectId, 
+      required: true ,
+      ref:"user"
+    },
+    productId: {
+      type:mongoose.Schema.ObjectId,
+      required: true ,
+      ref:"Product"
+    },
+    quantity: {
+          type: Number,
+          default: 1,
+    },
+    amount: { 
+      type: Number, 
+      required: true
+    },
+    address: { 
+      type: Object, 
+      required: true 
+    },
+    status: { 
+      type: String, 
+      default: "pending"
+     },
+    
+);
+
 module.exports={
     OrderModel
 }
