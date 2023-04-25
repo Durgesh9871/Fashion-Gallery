ee0337c184498929c388af1d6879a3087169d06d
Gautam Sonkar <gautamsonkar334@gmail.com>
2023-04-25 11:11:39 +0530

    gautam

--- Backend/routes/Order.js
+++ Backend/routes/Order.js
@@ -6,13 +6,12 @@ const { mongoose } = require("mongoose");
 const OrderRouter = require("express").Router();
 
 //  USER SIDE
+
 //********************** CREATE   Logged User only ***************************
 
 OrderRouter.post("/add",AddUserIdInCart, async (req, res) => {
   const userId=req.userId
-  
   try {
-
     const newOrder = new OrderModel({...req.body,userId});
     const savedOrder = await newOrder.save();
     res.status(200).send(savedOrder);
