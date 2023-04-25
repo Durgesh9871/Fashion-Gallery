64554cd44fa36daa083abf47fd112e1f0b3592b7
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-04-25 10:55:29 +0530

    Update Order.js

--- Backend/routes/Order.js
+++ Backend/routes/Order.js
@@ -4,3 +4,20 @@ const {AddUserIdInCart}=require("../Middlewares/AddUserIdInCart")
 const { mongoose } = require("mongoose");
 
 const OrderRouter = require("express").Router();
+
+//  USER SIDE
+//********************** CREATE   Logged User only ***************************
+
+OrderRouter.post("/add",AddUserIdInCart, async (req, res) => {
+  const userId=req.userId
+  
+  try {
+
+    const newOrder = new OrderModel({...req.body,userId});
+    const savedOrder = await newOrder.save();
+    res.status(200).send(savedOrder);
+  } 
+  catch (err){
+    res.status(500).send(err);
+  }
+});
